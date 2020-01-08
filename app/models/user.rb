948d676c0ac1c6a0c4cf694b87f3974f1678b68c
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-08 15:02:59 -0300

    Implement validations of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :email, presence: true
+    validates :password, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    has_one :account
 end
