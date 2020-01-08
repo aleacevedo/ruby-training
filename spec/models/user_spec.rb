c1c0752d25e945c710f5f65a28eb09f90cb17213
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-08 16:01:47 -0300

    Add some test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  let (:account) {FactoryGirl.create :account}
+  let (:email) {Faker::Internet.email}
+  let (:password) {Faker::Alphanumeric.alpha(number: 10)}
+  let (:first_name) {Faker::Name.first_name}
+  let (:last_name) {Faker::Name.last_name}
+  it "is valid with attributes" do
+    user = account.users.create(email: email, password: password, first_name: first_name, last_name: last_name)
+    expect(user).to be_valid
+  end
+  it "is not valid without email" do
+    user = account.users.create(email: nil, password: password, first_name: first_name, last_name: last_name)
+    expect(user).to_not be_valid
+  end
+  it "is not valid without password" do
+    user = account.users.create(email: email, password: nil, first_name: first_name, last_name: last_name)
+    expect(user).to_not be_valid
+  end
+  it "is not valid without first name" do
+    user = account.users.create(email: email, password: password, first_name: nil, last_name: last_name)
+    expect(user).to_not be_valid
+  end
+  it "is not valid without last name" do
+    user = account.users.create(email: email, password: password, first_name: first_name, last_name: nil)
+    expect(user).to_not be_valid
+  end
 end
