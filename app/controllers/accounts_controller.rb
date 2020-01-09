1e83f9e0edd590cf845789ebfc4376ce7e483b97
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-09 15:46:37 -0300

    Delete destroy method

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,27 +3,22 @@ class AccountsController < ApplicationController
         limit = params[:size] || 10
         offset = params[:page] ? (limit * page) : (limit * 0)
         @accounts = Account.limit(limit).offset(offset)
-        render json: @accounts, status: 200
+        render json: @accounts, status: :ok
     end
 
     def show
         @account = Account.find(params[:id])
-        render json: @account, status: 200
+        render json: @account, status: :ok
     end
 
     def create
-        @account = Account.create(params[:account])
-        render status: 201
+        @account = Account.create(params.require(:account).permit(:name))
+        render json: @account, status: :created
     end
 
     def update
-        Account.find(params[:id]).update_attributes(params[:account])
-        render status: 200
+        Account.find(params[:id]).update(params[:account].permit(:name))
+        render status: :ok
     end
 
-    def destroy
-        @patient = Patient.find(params[:id])
-        @patient.destroy
-        render status: 200
-    end
 end
