1769e9d0c3f2ee04368a73d13e24065f69ef98e3
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-09 09:10:44 -0300

    Add belongs to account to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     validates :password, presence: true
     validates :first_name, presence: true
     validates :last_name, presence: true
+    belongs_to :account
 end
