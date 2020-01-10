023325210390061044863049c29161aa0001c761
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-10 15:57:47 -0300

    Improve tests

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -3,6 +3,16 @@ require 'rails_helper'
 describe AccountsController, type: :controller do
   describe "GET #index" do
     let! (:accounts) {FactoryGirl.create_list(:account, 20)}
+    let (:account_schema) do
+    {
+      type: "object",
+      required: ["id", "name"],
+      properties: {
+        id: {"type" => "integer"},
+        name: {"type" => "string"}
+      }
+    }
+    end
     before do
         get :index
     end
@@ -10,61 +20,60 @@ describe AccountsController, type: :controller do
         expect(response).to have_http_status(:success)
     end
     it "JSON body response contains expected account attributes" do
-      json_response = JSON.parse(response.body)
-      json_response.each do |account|
-        expect(account.keys).to include("id", "name")
-      end
+      accounts_responded = response.parsed_body
+      expect(JSON::Validator.validate(account_schema, accounts_responded, list: true)).to be true
     end
-    it "get ten accounts" do
-      json_response = JSON.parse(response.body)
-      expect(json_response.length).to be(10)
+    it "responds with 10 accounts" do
+      accounts_responded = response.parsed_body
+      expect(accounts_responded.length).to be(10)
     end  
   end 
   describe "GET #show" do
     let! (:accounts) {FactoryGirl.create_list(:account, 20)}
     before do
-        get :show, params: {:id => accounts[0].id}
+        get :show, params: {id: accounts[0].id}
     end
     it "returns http success" do
         expect(response).to have_http_status(:success)
     end
     it "JSON body response contains expected account attributes" do
-      json_response = JSON.parse(response.body)
-      expect(json_response.keys).to include("id", "name")
+      account_responded = response.parsed_body
+      expect(account_responded.keys).to include("id", "name")
     end
     it "JSON body response contains match attributes" do
-      json_response = JSON.parse(response.body)
-      expect(json_response.keys).to include("id", "name")
-      expect(json_response["name"]).to match(accounts[0][:name])
+      account_responded = response.parsed_body
+      expect(account_responded.keys).to include("id", "name")
+      expect(account_responded["id"]).to eq accounts[0][:id]
+      expect(account_responded["name"]).to eq accounts[0][:name]
     end
   end 
   describe "POST #create" do
     let (:account) {FactoryGirl.attributes_for :account}
     before do
-      post :create, params: {:account => account}
+      post :create, params: {account: account}
     end
     it "returns http success" do
         expect(response).to have_http_status(:success)
     end 
     it "JSON body response match account attributes" do
-      json_response = JSON.parse(response.body)
-      expect(json_response.keys).to include("id", "name")
-      expect(json_response["name"]).to match(account[:name])
+      account_responded = response.parsed_body
+      expect(account_responded.keys).to include("id", "name")
+      expect(account_responded["name"]).to match(account[:name])
     end 
   end
   describe "PUT #update" do
     let (:account) {FactoryGirl.create :account}
     let (:new_account) {FactoryGirl.attributes_for :account}
     before do
-      put :update, params: {:account => new_account, :id => account.id}
+      put :update, params: {account: new_account, id: account.id}
     end
     it "returns http success" do
         expect(response).to have_http_status(:success)
     end 
     it "JSON body response match account attributes" do
-      get :show, params: {:id => account.id}
-      json_response = JSON.parse(response.body)
-      expect(json_response["name"]).to match(new_account[:name])
+      get :show, params: {id: account.id}
+      account_responded = response.parsed_body
+      expect(account_responded["name"]).to match(new_account[:name])
     end 
   end 
 end
