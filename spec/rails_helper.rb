030f2b67abe634dfa94bba9e2861824db72f329d
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-10 12:50:16 -0300

    Use factory bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'faker'
-require 'factory_girl_rails'
 require 'json'
+require 'factory_bot_rails'
 
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
