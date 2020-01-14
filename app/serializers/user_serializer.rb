0a792653ff818edc5408d787c88290c3bfe5c0ff
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-14 14:06:33 -0300

    Improve serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :account
-  def account
-    {
-      id: self.object.account.id,
-      name: self.object.account.name
-    }
-  end
+  attributes :id, :first_name, :last_name, :email
+  belongs_to :account
 end
