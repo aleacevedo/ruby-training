bda0c328433e78515a7854a642376e2c0d60bec7
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-15 14:07:44 -0300

    Implement authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,24 +5,23 @@ require 'jwt'
 class ApplicationController < ActionController::API
   protected
 
-  def authentication_data
-    @authentication_data ||= JWT.decode(request.headers['x-authorization'], nil, false)
-  rescue StandardError => _e
-    @authentication_data ||= {}
-  end
-
   def authenticate_user!
-    raise UnauthenticatedError if current_user.blank?
+    render status: :unauthorized if current_user.blank?
   end
 
   def current_user
     return @current_user if @current_user.present?
     return if request.headers['x-authorization'].blank?
 
-    user_data = authentication_data['user']
+    user_data = authentication_data[0]['data']
     return if user_data.blank?
-
-    @current_user = User.find_by(email: user_data['email'])
+    @current_user = User.find_by(email: user_data['user_email'])
     @current_user
+  end
+
+  def authentication_data
+    @authentication_data ||= JWT.decode(request.headers['x-authorization'], nil, false)
+  rescue StandardError => _e
+    @authentication_data ||= {}
   end
 end
