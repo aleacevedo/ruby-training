a377e5bf2bc186bbcb1247e2c81445118ed9c04a
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-15 15:41:42 -0300

    Add relations between account and shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,4 @@
 class Shop < ApplicationRecord
+  validates :name, presence: true
+  belongs_to :account, optional: false
 end
--- db/migrate/20200115182158_create_shops.rb
+++ db/migrate/20200115182158_create_shops.rb
@@ -1,8 +1,9 @@
 class CreateShops < ActiveRecord::Migration[6.0]
   def change
     create_table :shops do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :address
+      t.belongs_to :account
 
       t.timestamps
     end
