3fcb6b7426d4e71feec41485a5f3ed4dc1bdb663
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-15 14:06:46 -0300

    Create with authenticated user context

---
+++ spec/support/shared_examples/authenticated_user.rb
@@ -0,0 +1,12 @@
+RSpec.shared_context 'with authenticated user' do
+  let(:admin_user) { create(:user) }
+  let(:default_headers) do
+    {
+      'x-authorization' => JWT.encode(
+        { data: { user_id: admin_user.id, user_email: admin_user.email } },
+        nil,
+        'none'
+      )
+    }
+  end
+end
