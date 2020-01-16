bb8f04a4003e377c075a2fc0a7c79309249ee4aa
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-16 11:24:20 -0300

    Generate payment model

---
+++ db/migrate/20200116141827_create_payments.rb
@@ -0,0 +1,22 @@
+class CreatePayments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :payments do |t|
+      t.date :payment_date
+      t.date :origin_date
+      t.string :info_provider
+      t.string :info_provider
+      t.string :document_type
+      t.string :card_brando
+      t.string :clearing_number
+      t.decimal :total_amount
+      t.decimal, :total_deduction
+      t.decimal :total_earn
+      t.decimal :opening_balance
+      t.decimal :closing_balance
+      t.string :currency
+      t.boolean :is_balanced
+
+      t.timestamps
+    end
+  end
+end
