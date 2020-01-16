2c8c07165aba32297b27825667403b9491e4a6ac
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-16 11:07:38 -0300

    Implement some test for establishment model

--- spec/models/establishment_spec.rb
+++ spec/models/establishment_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Establishment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Establishment, type: :model do
+  let(:shop) { create :shop }
+  let(:number) { Faker::Number.number(digits: 20).to_s }
+  it 'is valid with attributes' do
+    establishment = Establishment.create(number: number, shop_id: shop.id)
+    expect(establishment).to be_valid
+  end
+
+  it 'is not valid without number' do
+    establishment = Establishment.create(shop_id: shop.id)
+    expect(establishment).to_not be_valid
+  end
+
+  it 'is not valid without shop_id' do
+    establishment = Establishment.create(number: number)
+    expect(establishment).to_not be_valid
+  end
 end
