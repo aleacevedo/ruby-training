ec9db4e5385f122694307762dbd7f1d0fbc05450
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-16 12:39:27 -0300

    Fix shop factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -20,11 +20,13 @@ FactoryBot.define do
   factory :shop do
     name { Faker::Company.name }
     address { Faker::Address.full_address }
+    account
   end
 end
 
 FactoryBot.define do
   factory :establishment do
     number { Faker::Number.number.to_s }
+    shop
   end
 end
