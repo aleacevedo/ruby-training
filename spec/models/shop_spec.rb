68d6057d855055f4d0e9d161abe3a1d613a6e54d
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-16 10:09:04 -0300

    Fix test of shop model

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
 
 describe Shop, type: :model do
+  let(:account) { create(:account) }
   let(:name) { Faker::Company.name }
   it 'is valid with valid attributes' do
-    account = Shop.new(name: name)
-    expect(account).to be_valid
+    shop = Shop.new(name: name, account_id: account.id)
+    expect(shop).to be_valid
   end
   it 'is not valid without a name' do
-    account = Shop.new
-    expect(account).to_not be_valid
+    shop = Shop.new(account_id: account.id)
+    expect(shop).to_not be_valid
+  end
+  it 'is not valid without a account id' do
+    shop = Shop.new(name: name)
+    expect(shop).to_not be_valid
   end
 end
