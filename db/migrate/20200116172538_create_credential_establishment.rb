079d5d85d84a9181c45736ed2b74eb6202283527
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-16 16:22:50 -0300

    Delete migration of association table

--- db/migrate/20200116172538_create_credential_establishment.rb
+++
@@ -1,8 +0,0 @@
-class CreateCredentialEstablishment < ActiveRecord::Migration[6.0]
-  def change
-    create_table :credential_establishments do |t|
-      t.references :credentials, null: false, foreign_key: true
-      t.references :establishment, null: false, foreign_key: true
-    end
-  end
-end
