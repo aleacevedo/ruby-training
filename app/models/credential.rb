50e8b8068b45ba576ca992c903823dccec6d19ac
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-16 12:57:00 -0300

    Implement credential's model and migration

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -1,2 +1,5 @@
 class Credential < ApplicationRecord
+  validates :login, presence: true, uniqueness: true
+  validates :password, presence: true
+  belongs_to :establishment
 end
--- db/migrate/20200116141056_create_credentials.rb
+++ db/migrate/20200116141056_create_credentials.rb
@@ -1,10 +1,13 @@
 class CreateCredentials < ActiveRecord::Migration[6.0]
   def change
     create_table :credentials do |t|
-      t.string :login
-      t.string :password
+      t.string :login, null: false
+      t.string :password, null: false
+
+      t.belongs_to :establishment
 
       t.timestamps
     end
+    add_index :credentials, :login, unique: true
   end
 end
