d5cf2a33ce232c52055b98a06ebd55564b6d64d0
Alejo Acevedo <alejoacevedo@Alejos-MBP.fibertel.com.ar>
2020-01-17 15:01:23 -0300

    Implement tests for payment

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Payment, type: :model do
+  let(:payment_attrs) { attributes_for :payment }
+  let(:establishment) { create :establishment }
+
+  before { payment_attrs.merge!(establishment_id: establishment.id) }
+
+  it 'is valid with valid attributes' do
+    payment = Payment.new(payment_attrs)
+    expect(payment).to be_valid
+  end
+
+  it 'is not valid with not valid provider' do
+    payment_attrs['provider'] = Faker::Number.between(from: 4, to: 100)
+    expect { Payment.new(payment_attrs) }.to raise_error(ArgumentError)
+  end
+
+  it 'is not valid with not valid document_type' do
+    payment_attrs['document_type'] = Faker::Number.between(from: 3, to: 100)
+    expect { Payment.new(payment_attrs) }.to raise_error(ArgumentError)
+  end
+
+  it 'is not valid with not valid currency' do
+    payment_attrs['currency'] = Faker::Number.between(from: 2, to: 100)
+    expect { Payment.new(payment_attrs) }.to raise_error(ArgumentError)
+  end
+
+  context "when payment alredy exist" do
+    let(:payment) { create(:payment) }
+    before { payment_attrs['clearing_number'] = payment.clearing_number }
+
+    it 'is not valid with not valid provider' do
+      expect(Payment.new(payment_attrs)).to_not be_valid
+    end
+  end
 end
