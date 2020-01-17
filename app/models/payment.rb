f57fc8745a6f6fa0cc7827e6f02ad255012e2da7
Alejo Acevedo <alejoacevedo@Alejos-MBP.fibertel.com.ar>
2020-01-17 15:00:28 -0300

    Declare enum and validations in payment model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,9 @@
 class Payment < ApplicationRecord
+  enum provider: %i[visa mastercard amex cabal]
+  enum document_type: %i[pdf txt csv]
+  enum currency: %i[usd ars]
+
   belongs_to :establishment
+
+  validates :clearing_number, presence: true, uniqueness: true
 end
