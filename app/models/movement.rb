631992403135f34960d27a6d1bfa3d83f07352b3
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-20 12:26:35 -0300

    Move installments validation to transaction model

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,17 +1,4 @@
 class Movement < ApplicationRecord
-  enum :provider %i[visa mastercard amex cabal]
-  enum :currency %i[usd ars]
-
-  validates :installments_current,
-            :installments_current,
-            :installments_total,
-            presence: true,
-            if: :any_installments?
-
-  def any_installments?
-    installments = [self[:installments_current],
-                    self[:installments_number],
-                    self[:installments_total]]
-    installments.any(&:present?)
-  end
+  enum provider: %i[visa mastercard amex cabal]
+  enum currency: %i[usd ars]
 end
