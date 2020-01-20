fb7787208fd7a10766597d0fd14bc171d100c7cc
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-20 11:29:00 -0300

    Make a custom validator

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,4 +1,17 @@
 class Movement < ApplicationRecord
   enum :provider %i[visa mastercard amex cabal]
   enum :currency %i[usd ars]
+
+  validates :installments_current,
+            :installments_current,
+            :installments_total,
+            presence: true,
+            if: :any_installments?
+
+  def any_installments?
+    installments = [self[:installments_current],
+                    self[:installments_number],
+                    self[:installments_total]]
+    installments.any(&:present?)
+  end
 end
