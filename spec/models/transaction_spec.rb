2a2c4ddd91b991ade7ccc0bc239d5dde3297e914
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-20 12:27:43 -0300

    Implement some tests

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:transaction_attrs) { attributes_for :transaction }
+  
+  it_behaves_like 'a movement'
+
+  it 'valid with valid transaction attributes' do
+    transaction = Transaction.new(transaction_attrs)
+    expect(transaction).to be_valid
+  end
+
+  it 'not valid with no installments current' do
+    transaction_attrs[:installments_current] = nil
+    transaction = Transaction.new(transaction_attrs)
+    expect(transaction).to_not be_valid
+  end
+
+  it 'not valid with no installments number' do
+    transaction_attrs[:installments_number] = nil
+    transaction = Transaction.new(transaction_attrs)
+    expect(transaction).to_not be_valid
+  end
+
+  it 'not valid with no installments total' do
+    transaction_attrs[:installments_total] = nil
+    transaction = Transaction.new(transaction_attrs)
+    expect(transaction).to_not be_valid
+  end
 end
