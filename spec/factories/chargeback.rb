531029939d2f094bec668e6075d925d96c0a157b
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-20 12:27:20 -0300

    Implement some factories

---
+++ spec/factories/chargeback.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :chargeback do
+    payment_date { Faker::Date.backward(days: 365) }
+    origin_date { Faker::Date.backward(days: 365) }
+    provider { Faker::Number.between(from: 0, to: 3) }
+    amount { Faker::Number.decimal }
+    currency { Faker::Number.between(from: 0, to: 1) }
+    payment
+    card_number { Faker::Number.number(digits: 16).to_str }
+    coupon_number { Faker::Number.number(digits: 6).to_str }
+    description_code { Faker::Number.number(digits: 2).to_str }
+  end
+end
--- spec/factories/movement.rb
+++ spec/factories/movement.rb
@@ -1,17 +1,14 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :transaction do
+  factory :movement do
     payment_date { Faker::Date.backward(days: 365) }
     origin_date { Faker::Date.backward(days: 365) }
     provider { Faker::Number.between(from: 0, to: 3) }
     amount { Faker::Number.decimal }
     currency { Faker::Number.between(from: 0, to: 1) }
     payment
-    installments_current { Faker::Number.number }
-    installments_number { Faker::Number.number }
-    installments_total { Faker::Number.number }
     card_number { Faker::Number.number(digits: 16).to_s }
-    discount_amount { Faker::Number.decimal }
+    coupon_number { Faker::Number.number(digits: 6).to_s }
   end
 end
