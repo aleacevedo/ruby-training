f8d1f049866caed97c4b36f99f0126e5eb6f712f
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-21 11:59:39 -0300

    Implement some helpers

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,15 +4,55 @@ class DashboardController < ApplicationController
   def index
     account = current_user.account
     render json: {
-      today_amount: calc_today_amount(account)
+      today_amount: calc_payment_amount_of(account, Date.today),
+      tomorrow_amount: calc_payment_amount_of(account, Date.tomorrow),
+      chargeback_refunds: calc_chargeback_and_refunds(account),
+      month: generate_month(account)
     }, status: 200
   end
 
   private
 
-  def calc_today_amount(account)
+  def calc_payment_amount_of(account, date)
     establishments = Establishment.where(shop: Shop.where(account: account))
     Payment.where(establishment: establishments)
-           .where(payment_date: Date.today).sum(&:total_amount)
+           .where(payment_date: date).sum(&:total_amount)
+  end
+
+  def calc_chargeback_and_refunds(account)
+    establishments = Establishment.where(shop: Shop.where(account: account))
+    Movement.where(payment: Payment.where(establishment: establishments))
+            .where(payment_date: Date.today)
+            .where('type=? OR type=?', 'Chargeback', 'Refund')
+            .sum(&:amount)
+  end
+
+  def generate_month(account)
+    month = {}
+    payments = generate_month_summary_payments(account)
+    transactions = generate_month_summary_transactions(account)
+    payments.keys.union(transactions.keys).each do |key|
+      month[key] = { payment_amount: payments[key], transaction_count: transactions[key] }
+    end
+    month
+  end
+
+  def generate_month_summary_payments(account)
+    establishments = Establishment.where(shop: Shop.where(account: account))
+    Payment.select(:payment_date)
+           .where(establishment: establishments)
+           .where('EXTRACT(MONTH FROM payment_date) = ?', Date.today.month)
+           .group('date(payment_date)')
+           .sum(:total_amount)
+  end
+
+  def generate_month_summary_transactions(account)
+    establishments = Establishment.where(shop: Shop.where(account: account))
+    Movement.select(:origin_date)
+            .where(payment: Payment.where(establishment: establishments))
+            .where(type: 'Transaction')
+            .where('EXTRACT(MONTH FROM origin_date) = ?', Date.today.month)
+            .group(:origin_date)
+            .count
   end
 end
