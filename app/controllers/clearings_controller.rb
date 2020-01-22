ccf68c1f64b0839ab3df1fc0fc283ecbbff4da18
Alejo Acevedo <alejoacevedo@Alejos-MacBook-Pro.local>
2020-01-22 10:45:40 -0300

    Implement controller

---
+++ app/controllers/clearings_controller.rb
@@ -0,0 +1,18 @@
+class ClearingsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    account = current_user.account
+    render json: calc_clearing(account), status: :ok
+  end
+
+  def calc_clearing(account)
+    from = params[:from] || Date.today.beginning_of_month
+    to = params[:to] || Date.today.end_of_month
+    establishments = Establishment.where(shop: Shop.where(account: account))
+    Payment.select(:total_amount)
+           .where(establishment: establishments)
+           .where('payment_date BETWEEN ? AND ?', from, to)
+           .group(:provider)
+           .sum(:total_amount)
+  end
+end
